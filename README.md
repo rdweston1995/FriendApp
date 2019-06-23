58d1012ccc10d7a4b4c1156fd8de7a24b4586424
Robert <robert.d.weston@gmail.com>
2019-06-22 19:42:19 -0700

    Updating README and apiRoutes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # FriendApp
+
+Site that the user may fill out a survey to find their potential new best friend based off their responses. Using their responses to find similar answers and how many different answers they have.
+
+Technologies Used:
+* NodeJS
+* JavaScript
+* HTML
+* CSS
+* NPM Express
+* NPM Path
+* Heroku Deployment
+* Bootstrap
+
+Author:
+* Robert Weston
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,11 @@
 // Your `apiRoutes.js` file should contain two routes:
+module.exports = function(app){
+    // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+    app.get("/api/friends", function(req, res){
+        
+    });
+    // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+    app.post("/api/friends", function(req, res){
 
-   // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-   // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+    });
+}
