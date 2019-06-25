7080ebb319eed5dbab84c8caca99de94665bfd6c
Robert <robert.d.weston@gmail.com>
2019-06-24 19:19:07 -0700

    init

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,10 +18,28 @@
 //    ]
 //}
 //```
+var friends = [
+    {
+        name: 'Tommy',
+        photo: '',
+        scores: [2, 4, 3, 5, 1, 2, 5, 2, 2, 3]
+    },{
+        name: 'Hannah',
+        photo: '',
+        scores: [5, 2, 3, 1, 3, 3, 2, 2, 5, 3]
+    },{
+        name: 'Zach',
+        photo: '',
+        scores: [3, 3, 3, 2, 3, 3, 2, 2, 4, 4]
+    },{
+        name: 'Antonio',
+        photo: '',
+        scores: [5, 3, 2, 2, 3, 3, 4, 1, 4, 5]
+    },{
+        name: 'Jordyn',
+        photo: '',
+        scores: [5, 1, 1, 1, 2, 3, 1, 2, 3, 2]
+    }
+];
 
-var newFriend = function(name, photo, scores){
-    this.route = name.split(" ").join("").toLowerCase();
-    this.name =  name;
-    this.photo = photo;
-    this.scores = scores;
-};
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,41 @@
+var friends = require('./../data/friends');
+
+console.log(friends);
 // Your `apiRoutes.js` file should contain two routes:
 module.exports = function(app){
     // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
     app.get("/api/friends", function(req, res){
-        
+        console.log("test");
+        res.json(friends);
     });
     // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
     app.post("/api/friends", function(req, res){
+        var match = {
+            name: '',
+            photo: '',
+            friendDiff: 51
+        };
+
+        var user = req.body;
+        var userScoes = user.scores;
+
+        for(ele in friends){
+            var totalDiff = 0;
+
+            for(index in friends[ele].scores.length){
+                totalDiff += Math.abs(parseInt(friends[ele].scores[index]) - parseInt(userScores[index]));
+            }
+
+            if(totalDiff <= match.friendDiff){
+                match.name = friends[ele].name;
+                match.photo = friends[ele].photo;
+                match.friendDiff = totalDiff;
+            }
+        }
+
+        friends.push(user);
+
+        res.json(match);
 
     });
 }
